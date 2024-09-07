cbb15ef517337d509063d8cc886da5413abb5cd1
EngrHamxa <118603129+EngrHamxa@users.noreply.github.com>
2024-09-07 16:15:37 +0000

    Nums,maths,date and time

---
+++ Basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(1));
+
+const numberone = 33.3912
+// console.log(numberone.toPrecision(3));
+
+const anothernumber = 120000
+// console.log(anothernumber.toLocaleString('en-IN'));
+
+// ++++++++++  Maths ++++++++++ //
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,6,7,8))
+// console.log(Math.max(4,6,7,8))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + 10)
