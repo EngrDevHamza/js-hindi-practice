a4dbfc9eb0e31f3fd681eed4b19feaa45ed9391f
EngrHamxa <118603129+EngrHamxa@users.noreply.github.com>
2024-09-07 09:23:11 +0000

    Compasision and strict check of operators

---
+++ Basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1);  datatype should be same
+
+// console.log(null > 0); => false
+// console.log(null == 0); => false
+// console.log(null >= 0); => true because comparision operator (> , <, = ) works differently and equality operator works differently
+
+// === strict check    it checks its datatype as well not only the value
+
+console.log("2" == 2); // => true
+console.log("2" === 2); // => false
