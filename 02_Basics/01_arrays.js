e8d6d81176f84b806f3c0ed21da0e7582338ff67
EngrHamxa <118603129+EngrHamxa@users.noreply.github.com>
2024-09-07 18:05:16 +0000

    Arrays Basics

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,40 @@
+// Array
+
+const myarr = [1, 2, 3, 4, 5, 6]
+const myHeros = ["Superman", "SpiderMan"]
+
+// const newarr = new Array(1,2,3,4,5)
+// console.log(myarr[0])
+// console.log(myHeros[1])
+// console.log(newarr[2])
+
+//Array Methods
+
+// myarr.push(7)
+// myarr.push(8)
+// myarr.pop()
+
+// myarr.unshift(9) 
+// myarr.shift()
+
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(5));
+
+
+// const newarr = myarr.join()
+
+// console.log(myarr);
+// console.log(typeof newarr);
+
+//Slice and Splice
+console.log("A ", myarr);
+
+const myn1 = myarr.slice(1, 3)
+console.log(myn1)
+console.log("B", myarr);
+
+const myn2 = myarr.splice(1, 3)
+console.log(myn2)
+console.log("C ", myarr);
+
---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,31 @@
+const TeamA = ["Superman", "Spiderman", "Ironman"]
+const TeamB = ["Safeguard", "Thor", "Prince"]
+
+// TeamA.push(TeamB)
+
+// console.log(TeamA);
+// console.log(TeamA[3][1]);
+
+// const TeamC = TeamA.concat(TeamB)
+// console.log(TeamC);
+
+// const TeamC = [...TeamA,...TeamB]
+// console.log(TeamC)
+
+// const anotherArry = [1, 2, 3, [4, 5, 6, [7, 8, 9]]]
+
+// const anotherNewArry = anotherArry.flat(Infinity)
+// console.log(anotherNewArry);
+
+console.log(Array.isArray("HAMZA"));
+console.log(Array.from("HAMZA"));
+console.log(Array.from({name:"HAMZA"})); //interesting keys say array banatay hain
+
+let score1 = 10
+let score2 = 20
+let score3 = 30
+
+console.log(Array.of(score1, score2, score3));
+
+
+
