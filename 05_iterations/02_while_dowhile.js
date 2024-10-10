b49f8303641dde518f84351dae8bb752d5e36003
Ninja_Dev <118603129+EngrDevHamza@users.noreply.github.com>
2024-10-10 21:10:06 +0000

    while,dowhile

---
+++ 05_iterations/02_while_dowhile.js
@@ -0,0 +1,19 @@
+// index = 0
+// while (index <=10) {
+//     // console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// let myarray = ["batman", "superman", "ironman"]
+// let arr = 0
+// while (arr < myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr =  arr + 1
+// }
+
+let score = 11
+
+do {
+    console.log(`score is: ${score}`);
+    
+} while (score<=10);
